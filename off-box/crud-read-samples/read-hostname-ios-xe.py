68f86564ec54dfe5b7a1fcfad0677db8d73d22c8
Mamdouh Elgamal <mselgamal2@gmail.com>
2018-11-29 17:19:09 +0000

    added comments

--- off-box/crud-read-samples/read-hostname-ios-xe.py
+++ off-box/crud-read-samples/read-hostname-ios-xe.py
@@ -8,13 +8,28 @@ import logging
 import sys
 import getpass
 
+'''
+    run:
+        pythong read-hostname-ios-xe.py <device ip address> <port>
+'''
 
+'''
+    access object's hostname attribute and print
+'''
 def read_hostname(native):
     print(native.hostname)
 
+'''
+   - format ip address, username and password to unicode format (required by netconf service provider)
+   - format port to int
+   - create a netconf provider object (handles connection to IOS device using netconf protocol over ssh)
+   - create a crud service object ('create, read, update and delete' service)
+   - create a ios_xe native object ('native' object represents 
+   the yang model for cisco IOS xe, converted to a python object)
+'''
 def main():
     ip = unicode(sys.argv[1],'utf-8')
-    port = 830
+    port = int(sys.argv[2])
     username = unicode(raw_input("Username: "),'utf-8') 
     password = unicode(getpass.getpass(),'utf-8')
     print("hello, we are connecting to the IOS device")
@@ -29,5 +44,5 @@ def main():
 
     native = xe_native.Native()
     native = crud.read(provider,native)
-    process_native(native)
+    read_hostname(native)
 main()
